c57a474d9c942c969f173a25dee64af8d27a5388
TiffanyYongNgikChee <g00425067@atu.ie>
2026-01-27 21:30:27 +0000

    add Spring Boot main application and server config

---
+++ spring-boot-api/src/main/java/com/fyp/hebench/HeBenchApplication.java
@@ -0,0 +1,11 @@
+package com.fyp.hebench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeBenchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HeBenchApplication.class, args);
+    }
+}
